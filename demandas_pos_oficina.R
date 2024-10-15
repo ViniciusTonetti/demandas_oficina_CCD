8f56a9eae2a0eb25b7f1d94a66b4584f75951515
ViniciusTonetti <vrtonetti@gmail.com>
2024-10-15 10:26:15 -0300

    assing code

--- demandas_pos_oficina.R
+++ demandas_pos_oficina.R
@@ -4,9 +4,12 @@
 # Cleaning directory
 rm(list = ls())
 
+# R messages in English
+Sys.setenv(LANG = "en_US.UTF-8")
+
 # pacotes
-#install.packages("wordcloud")
 library(wordcloud)
+library(tm)
 
 # Word Cloud com os termos das demandas mais sugeridas.
 
@@ -19,7 +22,7 @@ library(wordcloud)
 
 # Criando objeto com os termos usados nos fluxos de trabalho (FT):
 
-FT <- c(rep("Números e estatísticas", 1), rep("Mudanças climpáticas: riscos e adaptações", 3),
+FT <- c(rep("Números e estatísticas", 1), rep("Mudanças climáticas: riscos e adaptações", 3),
         rep("Mercado de biodiversidade", 3), rep("Modelos econômicos",3))
 
 wordcloud(words = FT)
