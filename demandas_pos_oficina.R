1d900e084839b86942be179b6b9fd92abbd30ec3
ViniciusTonetti <vrtonetti@gmail.com>
2024-10-15 10:24:24 -0300

    adding code

--- demandas_pos_oficina.R
+++ demandas_pos_oficina.R
@@ -1,2 +1,25 @@
 # Código para as demandas pós I Oficina CCD Estratégia Mata Atlântica
 # Vinicius Tonetti - vrtonetti@ufscar.br
+
+# Cleaning directory
+rm(list = ls())
+
+# pacotes
+#install.packages("wordcloud")
+library(wordcloud)
+
+# Word Cloud com os termos das demandas mais sugeridas.
+
+# "Números e estatísticas" - 1
+# "Modelos espaciais" - 0
+# "Mudanças climpáticas: riscos e adaptações" - 3
+# "Mercado de biodiversidade" - 3
+# "Modelos econômicos" - 3
+# "Priorização de áreas" - 0 
+
+# Criando objeto com os termos usados nos fluxos de trabalho (FT):
+
+FT <- c(rep("Números e estatísticas", 1), rep("Mudanças climpáticas: riscos e adaptações", 3),
+        rep("Mercado de biodiversidade", 3), rep("Modelos econômicos",3))
+
+wordcloud(words = FT)
